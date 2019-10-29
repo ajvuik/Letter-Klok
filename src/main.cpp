5264236770fe2aa75107814a997c4a4e3c5fb857
ajvuik <ajvuik@gmail.com>
2019-10-29 16:09:39 +0100

    Summer Winter time fix

--- src/main.cpp
+++ src/main.cpp
@@ -207,7 +207,7 @@ void loop(){
     if((Month==3 && Dow==0 && Day+7>31 && Hours>2)||
       (Month==3 && Dow>0 && Day+7>31)||
       (Month==10 && Dow==0 && Day+7>31 && Hours<2)||
-      (Month==10 && Day+7<31)||
+      (Month==10 && Day+(7-Dow)<31)||
       (Month>3 && Month<10)){
       if(Hours<24){
         Hours+=1;
